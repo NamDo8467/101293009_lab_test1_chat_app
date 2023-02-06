cd024824a5e81d89f7be816cf793dac7f440a5a4
NamDo8467 <huynhnamdo@gmail.com>
2023-02-06 16:10:05 -0500

    adding new routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Join from "./Join/Join";
 import Chat from "./Chat/Chat";
-
+import Signup from "./Signup/Signup";
+import Login from "./Login/Login";
+import './index.css'
 function App() {
-  
-
   return (
     <Router>
-      <Route exact path="/" component={Join} />
+      <Route exact path="/" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/join" component={Join} />
+
       <Route path="/chat"component={Chat} />
     </Router>
   );
