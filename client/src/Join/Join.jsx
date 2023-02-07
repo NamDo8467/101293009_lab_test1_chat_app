8fd9890e51b661b814ad47cd99c4ab984ce33977
NamDo8467 <huynhnamdo@gmail.com>
2023-02-06 21:04:14 -0500

    change the url that this component pushes user to

--- client/src/Join/Join.jsx
+++ client/src/Join/Join.jsx
@@ -1,47 +1,37 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import './Join.css'
+import React, { useState } from "react"
+import { useHistory, useLocation } from "react-router-dom"
+import "./Join.css"
 
-function Join({ location }) {
-  const [name, setName] = useState("");
-  const [room, setRoom] = useState("Javascript");
-  let history = useHistory();
-  const handleJoin = (e) => {
-    if (!name || !room) {
-      
-      e.preventDefault();
-    } else {
-      history.push(`/chat?name=${name}&room=${room}`);
-    }
-  };
-  return (
-    <form>
-      <div className="greeting">
-        <h1>Welcome to ChitChat</h1>
-      </div>
-      <div className="nameInput">
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Enter your name"
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div className="roomInput">
-        <select name="room" id="room" onChange={(e) => setRoom(e.target.value)}>
-          <option value="Javascript">Javascript</option>
-          <option value="Python">Python</option>
-          <option value="Ruby">Ruby</option>
-        </select>
-      </div>
-      <a className='button' type='submit' onClick={handleJoin} >JOIN</a>
-      {/* <button type="submit" onClick={handleJoin}>
-        JOIN
-      </button> */}
-    </form>
-  );
+function Join() {
+	const [room, setRoom] = useState("Javascript")
+
+	let history = useHistory()
+	const location = useLocation()
+	// console.log(location.state.name)
+	const handleJoin = e => {
+		if (!room) {
+			e.preventDefault()
+		} else {
+			history.push(`/chat`, { name: location.state.name, room })
+		}
+	}
+	return (
+		<form>
+			<div className='greeting m-2'>
+				<h1 className='font-bold text-2xl'>Welcome to ChitChat</h1>
+			</div>
+			<div className='roomInput mb-3'>
+				<select name='room' id='room' onChange={e => setRoom(e.target.value)}>
+					<option value='Javascript'>Javascript</option>
+					<option value='Python'>Python</option>
+					<option value='Ruby'>Ruby</option>
+				</select>
+			</div>
+			<button className='button' type='submit' onClick={handleJoin}>
+				JOIN
+			</button>
+		</form>
+	)
 }
 
-export default Join;
+export default Join
